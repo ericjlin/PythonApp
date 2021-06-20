60440889d68a4abbc35f1f48c0014d05355c9396
Colton <nearhorn@gmail.com>
2021-06-19 21:41:22 -0700

    feat: confirmed data is actually saving and tables are auto generated

--- main.py
+++ main.py
@@ -26,10 +26,25 @@ def connect_to_db():
     print(credentials['host'])
     db.bind(provider='cockroach', user=credentials['username'], password=credentials['password'],
             host=credentials['host'], database=credentials['database'], sslmode='disable', port=26257)
-    db.generate_mapping(create_tables=True)
-    db.set_sql_debug(True)
+    try:
+        db.generate_mapping(create_tables=True)
+        db.set_sql_debug(True)
+    except:
+        print('didnt work')
+
+@db_session
+def create_entities():
+    p1 = Person(name='John', age=20)
+    p2 = Person(name='Mary', age=22)
+    p3 = Person(name='Bob', age=30)
+    c1 = Car(make='Toyota', model='Prius', owner=p2)
+    c2 = Car(make='Ford', model='Explorer', owner=p3)
+    # there is no need to call commit() here -
+    # Pony does it automatically on leaving the db_session scope
+
+    print('created data in db')
 
 
 if __name__ == '__main__':
     connect_to_db()
-    # print("Hello 2")
+    create_entities()
